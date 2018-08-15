cd3fbb508a0298c6c27e8c9bba329c21b2fb9b92
rundef <bash88@gmail.com>
2018-08-15 17:52:41 -0400

    Travis => quiet composer update

--- tests/init-5.5.sh
+++ tests/init-5.5.sh
@@ -8,7 +8,7 @@ composer require "illuminate/support:5.5.*" --no-update -q
 composer require "orchestra/database:3.5.*" --no-update -q
 composer require "orchestra/testbench:3.5.*" --no-update -q
 composer require "phpunit/phpunit:6.0.*" --no-update -q
-composer update
+composer update -q
 composer dump-autoload
 
 mv -f composer-backup.json composer.json
--- tests/init-5.6.sh
+++ tests/init-5.6.sh
@@ -8,7 +8,7 @@ composer require "illuminate/support:5.6.*" --no-update -q
 composer require "orchestra/database:3.6.*" --no-update -q
 composer require "orchestra/testbench:3.6.*" --no-update -q
 composer require "phpunit/phpunit:7.0.*" --no-update -q
-composer update
+composer update -q
 composer dump-autoload
 
 mv -f composer-backup.json composer.json
