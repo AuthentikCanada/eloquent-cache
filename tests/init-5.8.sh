a016898e79f6b269a02b631d1760f708f0310b62
rundef <bash88@gmail.com>
2019-03-18 13:42:06 -0400

    Attempt to fix the Travis build

--- tests/init-5.8.sh
+++ tests/init-5.8.sh
@@ -7,7 +7,7 @@ composer require "illuminate/database:5.8.*" --no-update -q
 composer require "illuminate/support:5.8.*" --no-update -q
 composer require "orchestra/database:3.8.*" --no-update -q
 composer require "orchestra/testbench:3.8.*" --no-update -q
-composer require "phpunit/phpunit:8.0.*" --no-update -q
+composer require "phpunit/phpunit:7.5.*" --no-update -q
 composer require "php-coveralls/php-coveralls:2.1.*" --no-update -q
 composer require "mockery/mockery:1.0.*" --no-update -q
 composer update -q
