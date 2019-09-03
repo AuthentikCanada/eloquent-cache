f87fc63490ef362b6354b44428c01e1e081c711d
rundef <bash88@gmail.com>
2019-09-03 16:13:14 -0400

    Support for Laravel 6.0

---
+++ tests/init-6.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp composer.json composer-backup.json
+
+composer require "illuminate/cache:6.0.*" --no-update -q
+composer require "illuminate/database:6.0.*" --no-update -q
+composer require "illuminate/support:6.0.*" --no-update -q
+composer require "orchestra/database:4.0.*" --no-update -q
+composer require "orchestra/testbench:4.0.*" --no-update -q
+composer require "phpunit/phpunit:8.3.*" --no-update -q
+composer require "php-coveralls/php-coveralls:2.1.*" --no-update -q
+composer require "mockery/mockery:1.2.*" --no-update -q
+composer update -q
+composer dump-autoload
+
+mv -f composer-backup.json composer.json
