ef4c1175fb3c421a50a76f948cd7941a58a98307
rundef <bash88@gmail.com>
2019-09-03 16:46:17 -0400

    Fix for laravel 6

--- tests/init-6.0.sh
+++ tests/init-6.0.sh
@@ -2,15 +2,15 @@
 
 cp composer.json composer-backup.json
 
+composer require "phpunit/phpunit:8.3.*" --no-update -q
+composer require "orchestra/database:4.x-dev" --no-update -q
+composer require "orchestra/testbench:4.0.*" --no-update -q
 composer require "illuminate/cache:6.0.*" --no-update -q
 composer require "illuminate/database:6.0.*" --no-update -q
 composer require "illuminate/support:6.0.*" --no-update -q
-composer require "orchestra/database:4.0.*" --no-update -q
-composer require "orchestra/testbench:4.0.*" --no-update -q
-composer require "phpunit/phpunit:8.3.*" --no-update -q
 composer require "php-coveralls/php-coveralls:2.1.*" --no-update -q
 composer require "mockery/mockery:1.2.*" --no-update -q
-composer update -q
+composer update
 composer dump-autoload
 
 mv -f composer-backup.json composer.json
